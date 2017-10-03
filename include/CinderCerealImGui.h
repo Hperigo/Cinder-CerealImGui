242a8802638a2494613b0377bc256009986f0ea2
hperigo <hperigo@gmail.com>
2017-10-03 15:11:49 -0300

    removed unsed functions, added ColorA type

--- include/CinderCerealImGui.h
+++ include/CinderCerealImGui.h
@@ -36,51 +36,54 @@ namespace cereal {
             }
             
             Options(float _min, float _max, float _stepAmt  ) : mMin(_min), mMax(_max), mStepAmt(_stepAmt){
-                mHasStep = false;
                 mHasMinMax = true;
             }
             
             
+            bool hasMinMax(){ return mHasMinMax; }
+            float getMin() { return mMin; }
+            float getMax() { return mMax; }
+
             Options& setMinMax(float _min, float _max ){
                 mMin = _min;
                 mMax = _max;
+                mHasMinMax = true;
+                return *this;
+            }
+            Options& useMinMax(bool b){
                 mHasMinMax = false;
                 return *this;
             }
-            
+
+            float getStep(){ return mStepAmt; }
             
             Options& setStep( float amt ){
                 mStepAmt = amt;
                 return *this;
             }
             
-            float getMin(){
-                return mMin;
-            }
-            
-            float getMax(){
-                return mMax;
-            }
-            
-            float getStep(){
-                return mStepAmt;
-            }
             
             
-            bool hasStep(){
-                return mHasStep;
+            Options& setSpacing(float spacing){
+                mSpacing = spacing;
+                mHasSpacing = true;
+                return *this;
             }
             
-            bool hasMinMax(){
-                return mHasMinMax;
-            }
+            float getSpacing() { return mSpacing; }
+            
+            bool hasSpacing() { return mHasSpacing; }
+            
             
             
         private:
             
-            float mStepAmt = 0.1;
-            bool mHasStep = true;
+            
+            float mSpacing = 0;
+            bool mHasSpacing = false;
 
+            float mStepAmt = 0.1;
+            
             float mMin = 0;
             float mMax = 10.0;
             bool mHasMinMax = false;
@@ -97,36 +100,54 @@ namespace cereal {
         }
         
         template <class T> inline
-        void draw(const char * name, T & value){
+        void draw(std::string name, T & value){
+            
+            elementCount += 1;
+            
+            if(name == ""){
+                name = std::to_string(elementCount);
+            }
             
             auto opt = mElementOptions[name];
             
-            draw(name, value, opt);
+            
+            if( opt.hasSpacing() ){
+                ui::Dummy({ 0, opt.getSpacing() }  );
+            }
+            
+            draw(name.c_str(), value, opt);
+
         }
         
+        
+        void setOption(const std::string& elementName, Options option  ){
+            mElementOptions[elementName] = option;
+        }
+        
+        
+    private:
         
         void draw(const char * name, int & value, Options opt = Options() ){
-            
-            
-            if(opt.hasStep()){
-                ui::DragInt(name, &value, opt.getStep() );
-            }
-            else if(opt.hasMinMax()){
+         
+            if(opt.hasMinMax()){
                 ui::DragInt(name, &value, opt.getStep(), opt.getMin(), opt.getMax() );
-            }else{
-                ui::DragInt(name, &value);
             }
+            else{
+                ui::DragInt(name, &value, opt.getStep());
+            }
+            
         }
         
+        
         void draw(const char * name, float & value, Options opt = Options()){
-            if(opt.hasStep()){
-                ui::DragFloat(name, &value, opt.getStep() );
-            }
-            else if(opt.hasMinMax()){
+
+            if(opt.hasMinMax()){
                 ui::DragFloat(name, &value, opt.getStep(), opt.getMin(), opt.getMax() );
-            }else{
-                ui::DragFloat(name, &value);
             }
+            else{
+                ui::DragFloat(name, &value, opt.getStep());
+            }
+            
         }
         
         void draw(const char* name, bool& value, Options opt = Options()){
@@ -136,32 +157,27 @@ namespace cereal {
         
         void draw(const char* name, std::string& value, Options opt = Options()){
             ui::InputText(name, &value[0], 1000);
-            
         }
 
         
         void draw(const char* name, glm::vec2& value, Options opt = Options()){
             
-            if(opt.hasStep()){
-                ui::DragFloat2(name, &value[0], opt.getStep() );
-            }
-            else if(opt.hasMinMax()){
+            if(opt.hasMinMax()){
                 ui::DragFloat2(name, &value[0], opt.getStep(), opt.getMin(), opt.getMax() );
-            }else{
-                ui::DragFloat2(name, &value[0]);
+            }
+            else{
+                ui::DragFloat2(name, &value[0], opt.getStep());
             }
         }
         
         void draw(const char* name, glm::vec3& value,Options opt = Options()){
-            if(opt.hasStep()){
-                ui::DragFloat3(name, &value[0], opt.getStep() );
-            }
-            else if(opt.hasMinMax()){
+
+            if(opt.hasMinMax()){
                 ui::DragFloat3(name, &value[0], opt.getStep(), opt.getMin(), opt.getMax() );
-            }else{
-                ui::DragFloat3(name, &value[0]);
             }
-            
+            else{
+                ui::DragFloat3(name, &value[0], opt.getStep());
+            }
             
         }
         
@@ -170,20 +186,18 @@ namespace cereal {
             
             glm::vec3 euler = glm::eulerAngles(value);
             
-            if(ui::DragFloat3("quaternion", &euler[0])){
+            if(ui::DragFloat3(name, &euler[0], opt.getStep())){
                 value = glm::quat(euler);
             }
             
         }
         
         void draw(const char* name, ci::Color& value, Options opt = Options()){
-            
-            ui::ColorPicker3(name, &value[0]);
-            
+            ui::ColorEdit3(name, &value[0]);
         }
         
-        void setOption(const std::string& elementName, const Options& opt ){
-            mElementOptions[elementName] = opt;
+        void draw(const char* name, ci::ColorA& value, Options opt = Options()){
+            ui::ColorEdit4(name, &value[0]);
         }
         
         int elementCount = 0;
@@ -192,71 +206,39 @@ namespace cereal {
     
     
     
+    // ----
     template <class T> inline
-    void prologue( ImGuiArchive &, NameValuePair<T> const & )
-    { }
-    
+    void prologue( ImGuiArchive &, NameValuePair<T> const & ) { }
+
     template <class T> inline
-    void epilogue( ImGuiArchive &, NameValuePair<T> const & )
-    {
-    }
+    void epilogue( ImGuiArchive &, NameValuePair<T> const & ) { }
+
+    template <class T, traits::DisableIf<traits::has_minimal_base_class_serialization<T, traits::has_minimal_input_serialization, ImGuiArchive>::value ||
+    traits::has_minimal_input_serialization<T, ImGuiArchive>::value> = traits::sfinae> inline
+    void prologue( ImGuiArchive & ar, T const & ) {  }
     
     template <class T, traits::DisableIf<traits::has_minimal_base_class_serialization<T, traits::has_minimal_input_serialization, ImGuiArchive>::value ||
     traits::has_minimal_input_serialization<T, ImGuiArchive>::value> = traits::sfinae> inline
-    void prologue( ImGuiArchive & ar, T const & )
-    {
-        
-    }
+    void epilogue( ImGuiArchive & ar, T const & ) { }
     
-    template <class T, traits::DisableIf<traits::has_minimal_base_class_serialization<T, traits::has_minimal_input_serialization, ImGuiArchive>::value ||
-    traits::has_minimal_input_serialization<T, ImGuiArchive>::value> = traits::sfinae> inline
-    void epilogue( ImGuiArchive & ar, T const & )
-    {
-        
-    }
     
+    // ----
     template <class T> inline
     void CEREAL_LOAD_FUNCTION_NAME( ImGuiArchive & ar, NameValuePair<T> & t )
     {
-        ar.elementCount += 1;
         ar.draw<T>(t.name, t.value);
     }
-    
-    template <class T> inline
-    void CEREAL_LOAD_FUNCTION_NAME( ImGuiArchive & ar, NameValuePair<ci::Color> & t )
-    {
-        
-        ci::app::console() << "color..." << std::endl;
-        ar.elementCount += 1;
-        ar.draw<T>(t.name, t.value);
-    }
-
     
     template <class T, traits::EnableIf<std::is_arithmetic<T>::value> = traits::sfinae> inline
     void CEREAL_LOAD_FUNCTION_NAME(ImGuiArchive & ar, T & t)
     {
-        ar.elementCount += 1;
-        std::string name = "." + std::to_string(ar.elementCount);
-        ar.draw<T>(name.c_str(), t);
-        
+        ar.draw<T>("", t);
     }
-    // ######################################################################
+
     template<class CharT, class Traits, class Alloc> inline
     void CEREAL_LOAD_FUNCTION_NAME(ImGuiArchive & ar, std::basic_string<CharT, Traits, Alloc> & str)
     {
-        ar.elementCount += 1;
-        std::string name = "." + std::to_string(ar.elementCount);
-        ar.draw(name.c_str(), str);
-    }
-    
-    template<class CharT, class Traits, class Alloc> inline
-    void CEREAL_LOAD_FUNCTION_NAME(ImGuiArchive & ar, ci::Color& c)
-    {
-        ci::app::console() << "color" << std::endl;
-        
-        ar.elementCount += 1;
-        std::string name = "." + std::to_string(ar.elementCount);
-        ar.draw(name.c_str(), c);
+        ar.draw<std::string>("", str);
     }
     
 } // namespace cereal
